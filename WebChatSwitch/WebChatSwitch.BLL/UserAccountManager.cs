aef8f64a2dbf05c3832f82b87e86647d133ec6f4
Haobo <Haobo@DIWIN10>
2016-10-26 00:23:02 +0800

    Initialize the 3-level framework for MVC site

---
+++ WebChatSwitch/WebChatSwitch.BLL/UserAccountManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebChatSwitch.DAL;
+
+namespace WebChatSwitch.BLL
+{
+    public class UserAccountManager
+    {
+        private WebChatSwitchEntities context;
+
+        public WebChatSwitchEntities Context
+        {
+            get
+            {
+                return context;
+            }
+
+            set
+            {
+                context = value;
+            }
+        }
+        public UserAccountManager()
+        {
+            Context = new WebChatSwitchEntities();
+        }
+
+        public UserAccount GetUserAccountInfoByOpenId(string openId)
+        {
+            var userAccount = Context.UserAccounts.Include("Items").Include("Items.ItemPictures").FirstOrDefault(us => us.OpenId == openId);
+            return userAccount;
+        }
+    }
+}
