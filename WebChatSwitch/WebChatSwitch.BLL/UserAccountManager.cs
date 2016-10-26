a95d4cb0a9dc1fd8e8c87950da3bf0bde613f3b1
Haobo <Haobo@DIWIN10>
2016-10-27 00:27:27 +0800

    Add ItemManager for BLL functions

--- WebChatSwitch/WebChatSwitch.BLL/UserAccountManager.cs
+++ WebChatSwitch/WebChatSwitch.BLL/UserAccountManager.cs
@@ -7,22 +7,9 @@ using WebChatSwitch.DAL;
 
 namespace WebChatSwitch.BLL
 {
-    public class UserAccountManager
+    public class UserAccountManager : BaseManager
     {
-        private WebChatSwitchEntities context;
-
-        public WebChatSwitchEntities Context
-        {
-            get
-            {
-                return context;
-            }
-
-            set
-            {
-                context = value;
-            }
-        }
+        
         public UserAccountManager()
         {
             Context = new WebChatSwitchEntities();
@@ -30,7 +17,7 @@ namespace WebChatSwitch.BLL
 
         public UserAccount GetUserAccountInfoByOpenId(string openId)
         {
-            var userAccount = Context.UserAccounts.Include("Items").Include("Items.ItemPictures").FirstOrDefault(us => us.OpenId == openId);
+            var userAccount = Context.UserAccounts.Include("Items").Include("Items.ItemPictures").FirstOrDefault(ua => ua.OpenId == openId);
             return userAccount;
         }
     }
