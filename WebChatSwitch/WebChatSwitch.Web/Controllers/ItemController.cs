6c4537df4b3775160a9f8bc2ab48be0481ac4e7d
JDin13 <JDin13@LN04KZC47F3369>
2016-10-31 21:49:05 +0800

    Add Create View for Item

---
+++ WebChatSwitch/WebChatSwitch.Web/Controllers/ItemController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebChatSwitch.Web.Models;
+
+namespace WebChatSwitch.Web.Controllers
+{
+    public class ItemController : Controller
+    {
+        // GET: Item
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            ItemViewModel vm = new ItemViewModel();
+            vm.Available = true;
+            return View(vm);
+        }
+    }
+}
---
+++ WebChatSwitch/WebChatSwitch.Web/Models/ItemViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebChatSwitch.Web.Models
+{
+    public class ItemViewModel
+    {
+        [Required]
+        [Display(Name = "Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Display(Name = "Expectation")]
+        public string Expectation { get; set; }
+
+        [Display(Name = "Available")]
+        public bool Available { get; set; }
+    }
+}
