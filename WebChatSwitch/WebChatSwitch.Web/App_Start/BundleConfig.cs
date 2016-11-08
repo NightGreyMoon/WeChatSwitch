19cb9fb7fde9e56cbb9690a878beae05e6fe1ce6
babyvoxkl <wzc1984@gmail.com>
2016-11-08 18:19:44 +0800

    Fix some js issue

--- WebChatSwitch/WebChatSwitch.Web/App_Start/BundleConfig.cs
+++ WebChatSwitch/WebChatSwitch.Web/App_Start/BundleConfig.cs
@@ -14,11 +14,11 @@ namespace WebChatSwitch.Web
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            //bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            //          "~/Scripts/bootstrap.js"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css"));
+            //bundles.Add(new StyleBundle("~/Content/css").Include(
+            //          "~/Content/bootstrap.css"));
         }
     }
 }
